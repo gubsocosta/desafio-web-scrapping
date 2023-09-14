b3db090af46d84394c0bbae8640b5f59fba2fa79
Gabriel Costa <gubsocosta@gmail.com>
2023-09-14 15:08:00 -0300

    refactor: adjust service container (dependency injection)

--- app/Http/Controllers/GetCurrenciesInfoByIsoCodesController.php
+++ app/Http/Controllers/GetCurrenciesInfoByIsoCodesController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Core\Domain\UseCases\GetCurrenciesInfoByIsoCodes\GetCurrenciesInfoByIsoCodesInput;
 use Core\Domain\UseCases\GetCurrenciesInfoByIsoCodes\GetCurrenciesInfoByIsoCodesUseCase;
+use Exception;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Validator;
@@ -18,30 +19,22 @@ class GetCurrenciesInfoByIsoCodesController extends Controller
 
     public function __invoke(Request $request)
     {
-        $this->validateRequestInputs($request->all());
-        $isoCodeList = array_unique($request->input('isoCodes'));
-        try {
-            $result = $this->useCase->execute(new GetCurrenciesInfoByIsoCodesInput($isoCodeList));
-            return response()->json($result->currencyInfoList);
-        } catch (\Exception $e) {
-            Log::error('Error to process request: ' . $e->getMessage());
-            abort(500);
-        }
-    }
-
-    /**
-     * @param array $requestInputs
-     * @return void
-     */
-    private function validateRequestInputs(array $requestInputs): void
-    {
-        $validator = Validator::make($requestInputs, [
+        $validator = Validator::make($request->all(), [
             'isoCodes' => 'required|array|min:1',
             'isoCodes.*' => 'required|alpha|size:3'
         ]);
 
         if ($validator->fails()) {
-            response()->json(['errors' => $validator->errors()], 422);
+            return response()->json(['errors' => $validator->errors()], 422);
+        }
+
+        $isoCodeList = $request->input('isoCodes');
+        try {
+            $result = $this->useCase->execute(new GetCurrenciesInfoByIsoCodesInput($isoCodeList));
+            return response()->json($result->currencyInfoList);
+        } catch (Exception $e) {
+            Log::error('Error to process request: ' . $e->getMessage());
+            abort(500);
         }
     }
 }
--- app/Providers/GetCurrenciesInfoByIsoCodesProvider.php
+++ app/Providers/GetCurrenciesInfoByIsoCodesProvider.php
@@ -2,12 +2,16 @@
 
 namespace App\Providers;
 
+use App\Adapters\Caching\RedisCacheClient;
 use App\Adapters\CurrencyInfo\GetCurrenciesInfoByIsoCodesAdapter;
+use App\Adapters\CurrencyInfo\Services\CacheService;
+use App\Adapters\CurrencyInfo\Services\CrawlerService;
 use App\Adapters\Http\GuzzleHttpClient;
 use App\Adapters\Log\MonologLogger;
 use App\Http\Controllers\GetCurrenciesInfoByIsoCodesController;
 use Core\Domain\UseCases\GetCurrenciesInfoByIsoCodes\Gateways\GetCurrenciesInfoByIsoCodesGateway;
 use Core\Domain\UseCases\GetCurrenciesInfoByIsoCodes\GetCurrenciesInfoByIsoCodesUseCase;
+use Core\Infra\Caching\CacheClient;
 use Core\Infra\Http\HttpClient;
 use Core\Infra\Log\Logger;
 use Illuminate\Contracts\Foundation\Application;
@@ -23,8 +27,18 @@ class GetCurrenciesInfoByIsoCodesProvider extends ServiceProvider
         $this->app->bind(Logger::class, MonologLogger::class);
         $this->app->bind(GetCurrenciesInfoByIsoCodesGateway::class, GetCurrenciesInfoByIsoCodesAdapter::class);
         $this->app->bind(HttpClient::class, GuzzleHttpClient::class);
+        $this->app->bind(CacheClient::class, RedisCacheClient::class);
+        $this->app->bind(CacheService::class, function (Application $app) {
+            return new CacheService($app->make(CacheClient::class));
+        });
+        $this->app->bind(CrawlerService::class, function (Application $app) {
+            return new CrawlerService($app->make(HttpClient::class));
+        });
         $this->app->bind(GetCurrenciesInfoByIsoCodesAdapter::class, function (Application $app) {
-            return new GetCurrenciesInfoByIsoCodesAdapter($app->make(HttpClient::class));
+            return new GetCurrenciesInfoByIsoCodesAdapter(
+                $app->make(CacheService::class),
+                $app->make(CrawlerService::class),
+            );
         });
         $this->app->bind(GetCurrenciesInfoByIsoCodesUseCase::class, function (Application $app) {
             return new GetCurrenciesInfoByIsoCodesUseCase(
